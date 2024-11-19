82a9fb576f523eb08ae7a1b8f7e74ca3085c83fc
bilalshafqat42 <bilalshafqat42@gmail.com>
2024-11-19 10:18:58 +0400

    api works between server and client

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
+);
 
 app.get("/test", (req, res) => {
   res.json("text ok");
